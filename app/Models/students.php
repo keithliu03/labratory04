81697239edb26c8e6af670e5a4dc5c866ed39d90
BCastJr <benedictcastresjr@gmail.com>
2019-01-31 11:29:23 -0800

    added student and classes models

---
+++ app/Models/students.php
@@ -0,0 +1,24 @@
+<?php 
+
+use CodeIgniter\Model;
+
+class Students extends Model
+{
+	protected $data      = [
+'Cassidy' => ['id'=>'a00986001', 'fullname' => 'Cassidy Banks', 'year'=> 2],
+'Malcolm' => ['id'=>'a00986002', 'fullname' => 'Malcolm Longboi', 'year'=> 3],
+'Ernie' => ['id'=>'a00986003', 'fullname' => 'Ernie Smallman', 'year'=>1]
+];
+	    public function find($id = null)
+	    {
+	        return $this->data[$id];
+	    }
+	
+	    public function findAll(int $limit = 0, int $offset = 0)
+	    {
+	        return $this->data;
+	    }
+
+}
+
+?>
